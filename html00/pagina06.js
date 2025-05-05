b3359be62431578292b3e24dd317ef8f2411badb
leonardoruizp <leorp3101@gmail.com>
2025-05-05 16:29:43 -0600

    06-eventos

---
+++ html00/pagina06.js
@@ -0,0 +1,22 @@
+const Picture = (id) => {
+    return `<figure >
+    <img src="https://picsum.photos/id/${id}/200/200" alt="" class="redonda">
+    <figcaption>Jhon</figcaption>
+</figure>`
+}
+const p = document.getElementById("app")
+p.innerHTML = Picture(1) + Picture(2) + Picture(3)
+
+const mensaje = (id) => {alert ('holaaa' + id)}
+
+const lista = app.querySelectorAll('img')
+console.log (lista)
+for (let i = 0; i < lista.length; i++) {
+    console.log(lista[i])
+    lista[i].addEventListener("click", 
+        () => {
+            lista[i].classList.toggle("redonda")
+        }
+    )
+    
+}
