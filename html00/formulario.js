04d935fab6bd36fccf3227b3fff5c265d4aee374
leonardoruizp <leorp3101@gmail.com>
2025-05-08 16:57:50 -0600

    09-10 modulos

--- html00/formulario.js
+++ html00/formulario.js
@@ -20,4 +20,13 @@ boton.addEventListener("click",
 
     console.log(user.value)
     console.log(password.value)
-})
+
+    user.setAttribute("name", "q")
+    user.setAttribute("password", "yyy")
+
+    let formulario = document.getElementsByTagName("form")
+    formulario[0].setAttribute("action","https://www.google.com/search")
+    console.log(formulario[0])
+    formulario[0].submit()
+    }
+)
---
+++ html00/xM.js
@@ -0,0 +1,11 @@
+import {y} from './yM.js'
+function x(params) {
+    y("x")
+    console.log("x:" + params)
+    return "fin x"
+}
+function xx(params) {
+    "fin xx"
+}
+export {x}
+export {xx}
